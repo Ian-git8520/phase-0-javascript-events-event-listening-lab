3f973164fbfef7020074fa4a85c5e7110078b019
ian_git8520 <iandavidnjenga@gmail.com>
2025-09-01 00:21:44 +0300

    event listening

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+  const input = document.querySelector("input");
+
+  input.addEventListener("click", function (event) {
+    console.log("clicked");
+  });
+
+  const form = document.getElementById("myForm");
+
+  if (form) {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      alert("form submitted");
+    });
+  }
 }
+
+module.exports = { addingEventListener };
